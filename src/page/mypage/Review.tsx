ef72e1431aef865c189833c16e43bd6564a4a3e2
Kim Ji Yu <spbabo97@naver.com>
2024-04-09 03:19:44 +0900

    Feat : 로그아웃 수정

--- src/page/mypage/Review.tsx
+++ src/page/mypage/Review.tsx
@@ -3,8 +3,8 @@ import Navigation from "../../components/common/menu/Navigation";
 import { ReactComponent as SettingSVG } from "../../assets/image/icon/setting.svg";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { ReactComponent as Vintage } from "../../assets/image/icon/vintage.svg";
-import { useAppSelector } from "../../store/hooks";
-import FormNickname from "../../components/mypage/FormNickname";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import FormNickname from "../../components/mypage/Information/FormNickname";
 import axios from "axios";
 import ComingModal from "../../components/mypage/Modal/ComingModal";
 import { Link } from "react-router-dom";
@@ -12,6 +12,8 @@ import ReviewPage from "../../components/mypage/ReviewPage";
 import InfiniteScroll from "react-infinite-scroller";
 import { useMypageTotal } from "../../hook/useMypageTotal";
 import Spinner from "../../components/common/spinner/Spinner";
+import { logout } from "../../store/features/auth/authSlice";
+import { ReactComponent as LogoutSVG} from "../../assets/image/icon/logout.svg"
 
 export interface RootInterface {
   reviewTotalCount: number;
@@ -29,6 +31,9 @@ export interface Review {
 }
 
 export default function BookMark() {
+
+  const dispatch = useAppDispatch();
+
   // modal
   const [isOpen, setIsOpen] = useState(false);
   const onClose = () => {
@@ -44,45 +49,42 @@ export default function BookMark() {
   const [hasNext, setHasNext] = useState(true);
   const [page, setPage] = useState(1);
 
-  const loadMore = useCallback(() => {
+  const loadMore = useCallback(()=>{
     axios(`/api/v1/user/my/reviews?page=${page}&size=10`, {
       headers: {
         Authorization: userInfo?.token,
       },
     })
-      .then(({ data }: { data: RootInterface }) => {
-        if (data.hasNext) {
-          setHasNext(data.hasNext);
-          setReivews((prev) => [...prev, ...data.reviews]);
-          setPage(page + 1);
-        } else {
-          return;
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+    .then(({ data }: { data: RootInterface }) => {
+      setHasNext(data.hasNext);
+      setReivews((prev) => [...prev, ...data.reviews]);
+      setPage(page + 1);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  },[page])
 
   return (
     <>
       <main className="h-screen">
         <div className="flex h-[calc(100vh-70px)] flex-col">
           <div className="flex-none">
-            <TopBar hide={true}>
-              <SettingSVG
-                onClick={() => setIsOpen(true)}
-                className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer fill-gray-800"
-              />
-            </TopBar>
-
-            <div className="flex items-center px-6 font-pretendard leading-none tracking-custom">
-              <div className="relative h-[68px] w-[68px] rounded-full bg-[#EBEBEB]">
-                <Vintage className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+            
+            <div className="flex items-center justify-between px-6 font-pretendard leading-none tracking-custom mt-16">
+              <div className="flex items-center">
+                <div className="relative h-[68px] w-[68px] rounded-full bg-[#EBEBEB]">
+                  <Vintage className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+                </div>
+                <div className="ml-4">
+                  <FormNickname />
+                </div>
               </div>
-              <div className="ml-4">
-                <FormNickname />
-              </div>
+              <button
+                onClick={() => dispatch(logout())}
+              >
+                <LogoutSVG/>
+              </button>
             </div>
 
             <div className="mt-6 flex border-b border-gray-300 text-center text-[13px] font-semibold text-gray-300">
@@ -103,15 +105,10 @@ export default function BookMark() {
 
           <div className="overflow-y-auto h-full scrollbar-hide">
             <InfiniteScroll
-              pageStart={1}
+              pageStart={0}
               loadMore={loadMore}
               hasMore={hasNext}
-              loader={<div className='text-center' key={0}>로딩중입니다...</div>}
-              // loader={
-              //   <div className="flex h-96 w-full items-center justify-center">
-              //     <Spinner />
-              //   </div>
-              // }
+              loader={<div className="text-center" key={0}><Spinner/></div>}
               useWindow={false}
             >
               {reivews.map((reivew, i) => (
